f8d362346d2ccc258d53f9ddac3d71c1af0a2feb
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-05 12:06:51 -0500

    asdf

--- reference_practice.cpp
+++ reference_practice.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
 using namespace std;
 
-
+void change(double num);
 
 int main (){
 
-
+	double a;
+	
+	cout << "Enter a number you wish to change." << endl;
+	cin >> a
+	change(a)
 
 
 	return 0;
+}
+
+void change(double num){
+	num = (num%13+74)/60*17
 }
