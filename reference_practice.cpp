e6e7d67a757aa896d2e05ca1d2cae79cad4b1458
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-05 12:13:19 -0500

    asdf

--- reference_practice.cpp
+++ reference_practice.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
-void change(double num);
+void change(double & num);
 
 int main (){
 
 	double a;
 	
 	cout << "Enter a number you wish to change." << endl;
-	cin >> a
-	change(a)
+	cin >> a;
+	change(a);
+	
+	cout << "Your number is now: " << a << endl;
 
 
 	return 0;
 }
 
-void change(double num){
-	num = (num%13+74)/60*17
+void change(double & num){
+	num = (fmod(num,13)+74)/60*17;
 }
