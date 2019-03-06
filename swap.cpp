6e95ce55c4c6c95c541766068c38053561e8c80b
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-06 13:49:14 -0500

    asdf

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+double swapper();
+
+int main (){
+	double first_number, second_number;
+	cout << "Enter your first number" << endl;
+	cin >> first_number;
+	cout << "Enter your second number" << endl;
+	cin >> second_number;
+	
+	return 0;
+}
+
+double swapper() {
+
+
+}
