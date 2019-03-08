555cde07be86cb95a613c35884e21b49d09e0abb
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-08 10:00:12 -0500

    asdf

--- complex_calculator.cpp
+++ complex_calculator.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //A calculator of complex numbers
 //A test of my ability to utilize functions
 //this calculator can:Add, subtract, divide, multiply, find the length, and get new inputs
-//
+//Seven functions: Addition, subtraction, division, multiplications, length, inputs, and outputs
 
 int main (){
 	
