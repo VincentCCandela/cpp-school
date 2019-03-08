1f130e678ff28a8b6ec317be3a6258d99381e52a
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-08 09:51:48 -0500

    overloaded functions

---
+++ complex_calculator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+//A calculator of complex numbers
+//A test of my ability to utilize functions
+
+
+int main (){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
