7aa93801e3538193664582fdc1068fbf4f2cb9d6
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-08 10:20:24 -0500

    asdf

--- complex_calculator.cpp
+++ complex_calculator.cpp
@@ -10,12 +10,16 @@ using namespace std;
 
 
 // prototypes go here
+
+void input(double & real, double & imag);
 int main(void){
+
 	char choice;
 	double A = 0;
 	double B = 0;
 	double C = 0;
 	double D = 0;
+	
 	cout << "Welcome to the Complex Number Calculator!" << endl;
 	cout << "Here are your options: " << endl;
 	cout << "+ (addition)" << endl;
@@ -25,24 +29,55 @@ int main(void){
 	cout << "l (length)" << endl;
 	cout << "i (new input)" << endl;
 	cout << "q (quit)" << endl;
+	
 	while(choice != 'q'){
 		cout << "Enter what you want to do: ";
 		cin >> choice;
+		
 		switch(choice){
+		
 			case 'i':
-			break;
+				input(A,B)
+				//output A and B
+				//cout << A << " " << B << endl;
+				break;
+				
 			case '+':
-			break;
+				//input C and D
+				//add A B, C D
+				//output the new A B
+				break;
+				
 	// complete rest of cases here
+	
 			case 'q':
-			cout << "Thanks for using our 
-			calculator!" << endl;
-			break;
+				cout << "Thanks for using our calculator!" << endl;
+				break;
+				
 			default:
-			cout << "Not a valid choice!" << endl;
-			break;
+				cout << "Not a valid choice!" << endl;
+				break;
 		}
 	}
 return 0;
 }
 // function definitions
+
+void input(double & real, double & imag){
+	cout << "Enter the real number" << endl;
+	cin >> real;
+	cout << "Enter the imaginary number" << endl;
+	cin >> imag;
+}
+
+
+
+
+
+
+
+
+
+
+
+
