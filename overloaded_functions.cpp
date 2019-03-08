57e137a42962a7f368f21c42895724cfdbb712b6
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-08 09:48:52 -0500

    overloaded functions

---
+++ overloaded_functions.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+	//functions that have the same return type and name but with different parameters
+	
+	int add(int num1, int num2, int num3);
+	int add(double num1, double num2);
+	
+	cout << add(10.2,2.5) << endl; // The one with two parameters will be called because of two parameters are supplied
