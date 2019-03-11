cae7795299b22c9f9802b0b7fe26b20acd4b61c8
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-11 13:11:32 -0400

    nearly the final commit for the complex calculator

--- complex_calculator.cpp
+++ complex_calculator.cpp
@@ -11,10 +11,13 @@ using namespace std;
 
 // prototypes go here
 
-void input(double & real, double & imag);
-void output(double & real, double & imag);
+void input(double & real1, double & imag1);					//the prototypes of the all the functions used to calculate the numbers for this program
+void output(double & real1, double & imag1);
 void addition(double & real1, double & imag1, double real2, double imag2);
 void subtraction(double & real1, double & imag1, double real2, double imag2);
+void multiplication(double & real1, double & imag1, double real2, double imag2);
+void division(double & real1, double & imag1, double real2, double imag2);
+void length(double & rea1l, double & imag1);
 
 int main(void){
 
@@ -24,7 +27,7 @@ int main(void){
 	double C = 0;
 	double D = 0;
 	
-	cout << "Welcome to the Complex Number Calculator!" << endl;
+	cout << "Welcome to the Complex Number Calculator!" << endl;	//the startup printout the lists the available operations to the user
 	cout << "Here are your options: " << endl;
 	cout << "+ (addition)" << endl;
 	cout << "- (subtraction)" << endl;
@@ -34,30 +37,40 @@ int main(void){
 	cout << "i (new input)" << endl;
 	cout << "q (quit)" << endl;
 	
-	while(choice != 'q'){
+	while(choice != 'q'){				//switch statements that let the user decide which operations they want to employ
 		cout << "Enter what you want to do: ";
 		cin >> choice;
 		
 		switch(choice){
 		
-			case 'i':
+			case 'i':			//the input operation
 				input(A,B);
 				output(A,B);
 				break;
 				
-			case '+':
+			case '+':			//the addition operation
 				input(C,D);
 				addition(A,B,C,D);
 				output(A,B);
 				break;
-			case '-':
+			case '-':			//the 
 				input(C,D);
 				subtraction(A,B,C,D);
 				output(A,B);
 				break;
-				
-	// complete rest of cases here
-	
+			case '*':
+				input(C,D);
+				multiplication(A,B,C,D);
+				output(A,B);
+				break;
+			case '/':
+				input(C,D);
+				division(A,B,C,D);
+				output(A,B);
+				break;
+			case 'l':
+				length(A,B);
+				break;
 			case 'q':
 				cout << "Thanks for using our calculator!" << endl;
 				break;
@@ -71,15 +84,15 @@ return 0;
 }
 // function definitions
 
-void input(double & real, double & imag){
+void input(double & real1, double & imag1){
 	cout << "Enter the real number" << endl;
-	cin >> real;
+	cin >> real1;
 	cout << "Enter the imaginary number" << endl;
-	cin >> imag;
+	cin >> imag1;
 }
 
-void output(double & real, double & imag){
-	cout << real << " + " << imag << "i" << endl;	
+void output(double & real1, double & imag1){
+	cout << fixed << setprecision(2) << real1 << " + " << imag1 << "i" << endl;	
 	
 }
 void addition(double & real1, double & imag1, double real2, double imag2){
@@ -92,9 +105,20 @@ void subtraction(double & real1, double & imag1, double real2, double imag2){
 
 }
 
+void multiplication(double & real1, double & imag1, double real2, double imag2){
+	real1 = real1 * real2 - imag1 * imag2;
+	imag1 = real1 * imag2 + real2 * imag1;
+}
 
+void division(double & real1, double & imag1, double real2, double imag2){
+	real1 = (real1 * real2 + imag1 * imag2) / (real2 * real2 + imag2 * imag2);
+	imag1 = (imag1 * real2 - real1 *  imag2) / (real2 * real2 + imag2 * imag2);
+}
 
-
+void length(double & real1, double & imag1){
+	double the_length = sqrt( real1 * real1 + imag1 * imag1 );
+	cout << fixed << setprecision(2) << "The length is: " << the_length << endl;
+}
 
 
 
