a1d3979691298f9c1e0aedb2f9349ee44822d08b
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-19 09:57:37 -0400

    struct practice

--- struct_practice.cpp
+++ struct_practice.cpp
@@ -13,6 +13,19 @@ struct pokemon{
 }
 
 int main (){
+	pokemon Jed;
+	cout << "What is your pokemon's name?" << endl;
+	cin >> Jed.name;
+	cout << "What is your pokemon's type?" << endl;
+	cin >> Jed.type;
+	cout << "What is your pokemon's hp?" << endl;
+	cin >> Jed.hp;
+	cout << "What is your pokemon's level?" << endl;
+	cin >> Jed.level;
+	cout << "What is your pokemon's attack?" << endl;
+	cin >> Jed.attack;
+	cout << "Is your pokemon shaggy? (y/n)" << endl;
+	cin >> Jed.is_shaggy;
 
 
 	return 0;
