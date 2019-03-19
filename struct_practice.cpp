2f11faf75bb38e81d9a5ce069482a1da06ad0e12
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-19 10:31:19 -0400

    af

--- struct_practice.cpp
+++ struct_practice.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 
-struct pokemon{
+struct pokemon {
 	string name;
 	string type;
 	int hp;
@@ -12,7 +12,22 @@ struct pokemon{
 	char is_shaggy;
 };
 
+struct program {
+	string name;
+	char fast;
+	char common;
+};
+
 int main (){
+	program one;
+	
+	cout << "What is the name of your program" << endl;
+	cin >> one.name;
+	cout << "Is your language fast? (y/n)" << endl;
+	cin >> one.fast;
+	cout << "Is it a common language? (y/n)" << endl;
+	cin >> one.common;
+	
 	pokemon Jed;
 	pokemon Dan;
 	
@@ -26,12 +41,22 @@ int main (){
 	cin >> Jed.level;
 	cout << "What is your pokemon's attack?" << endl;
 	cin >> Jed.attack;
-	cout << "Is your pokemon shaggy? (y/n)" << endl;
-	cin >> Jed.is_shaggy;
+	
+	while (Jed.is_shaggy != 'y' && Jed.is_shaggy != 'Y'  &&  Jed.is_shaggy != 'n' && Jed.is_shaggy != 'N') {
+		cout << "Is your pokemon shaggy? (y/n)" << endl;
+		cin >> Jed.is_shaggy;
+	}
 	
 	cout << endl;
 	
 	cout << Jed.name << "'s type is: " << Jed.type << "and it's hp, level, and attacks are: " << Jed.hp << " " << Jed.level << " " << Jed.attack << endl;
+
+	if (Jed.is_shaggy == 'y' || Jed.is_shaggy == 'Y'){
+		cout << "Your pokemon is a shaggy." << endl;
+	}
+	else {
+		cout << "Your pokemon is not a shaggy." << endl;
+	}
 
 	cout << "What is your pokemon's name?" << endl;
 	cin >> Dan.name;
@@ -43,8 +68,23 @@ int main (){
 	cin >> Dan.level;
 	cout << "What is your pokemon's attack?" << endl;
 	cin >> Dan.attack;
-	cout << "Is your pokemon shaggy? (y/n)" << endl;
-	cin >> Dan.is_shaggy;
+
+	while (Dan.is_shaggy != 'y' && Dan.is_shaggy != 'Y'  &&  Dan.is_shaggy != 'n' && Dan.is_shaggy != 'N') {
+		cout << "Is your pokemon shaggy? (y/n)" << endl;
+		cin >> Dan.is_shaggy;
+	}
+
+	cout << endl;
+	
+	cout << Dan.name << "'s type is: " << Dan.type << "and it's hp, level, and attacks are: " << Dan.hp << " " << Dan.level << " " << Dan.attack << endl;
+
+	if (Dan.is_shaggy == 'y' || Dan.is_shaggy == 'Y'){
+		cout << "Your pokemon is a shaggy." << endl;
+	}
+	else {
+		cout << "Your pokemon is not a shaggy." << endl;
+	}
+
 
 	cout << Dan.name << "'s type is: " << Dan.type << "and it's hp, level, and attacks are: " << Dan.hp << " " << Dan.level << " " << Dan.attack << endl;
 
