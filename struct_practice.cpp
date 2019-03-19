0811e162b15b237d1c121759bce5ab7af82931e6
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-19 10:03:55 -0400

    struct practice

--- struct_practice.cpp
+++ struct_practice.cpp
@@ -10,10 +10,12 @@ struct pokemon{
 	int level;
 	string attack;
 	char is_shaggy;
-}
+};
 
 int main (){
 	pokemon Jed;
+	pokemon Dan;
+	
 	cout << "What is your pokemon's name?" << endl;
 	cin >> Jed.name;
 	cout << "What is your pokemon's type?" << endl;
@@ -26,7 +28,25 @@ int main (){
 	cin >> Jed.attack;
 	cout << "Is your pokemon shaggy? (y/n)" << endl;
 	cin >> Jed.is_shaggy;
+	
+	cout << endl;
+	
+	cout << Jed.name << "'s type is: " << Jed.type << "and it's hp, level, and attacks are: " << Jed.hp << " " << Jed.level << " " << Jed.attack << endl;
 
+	cout << "What is your pokemon's name?" << endl;
+	cin >> Dan.name;
+	cout << "What is your pokemon's type?" << endl;
+	cin >> Dan.type;
+	cout << "What is your pokemon's hp?" << endl;
+	cin >> Dan.hp;
+	cout << "What is your pokemon's level?" << endl;
+	cin >> Dan.level;
+	cout << "What is your pokemon's attack?" << endl;
+	cin >> Dan.attack;
+	cout << "Is your pokemon shaggy? (y/n)" << endl;
+	cin >> Dan.is_shaggy;
+
+	cout << Dan.name << "'s type is: " << Dan.type << "and it's hp, level, and attacks are: " << Dan.hp << " " << Dan.level << " " << Dan.attack << endl;
 
 	return 0;
 }
