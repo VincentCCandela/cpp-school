ef9d02af5707503e0d0e31d34bb66943ae6b234e
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-19 09:55:06 -0400

    struct practice

---
+++ struct_practice.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+
+struct pokemon{
+	string name;
+	string type;
+	int hp;
+	int level;
+	string attack;
+	char is_shaggy;
+}
+
+int main (){
+
+
+	return 0;
+}
