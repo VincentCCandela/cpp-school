c5e44104854eb86a145decb1cc617f17d07aa36a
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-20 13:16:07 -0400

    asdfasdf

--- arrays.cpp
+++ arrays.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-	double gpas[8] = {3.4,34,456,235,4357,1234,3546,4321335};
+	const int size = 20;
+
+	double gpas[size] = {3.4,34,456,235,4357,1234,3546,4321335};
 	cout << "gpas[1] is: " << gpas[1] << endl;
 	
-	for (int i = 0; i < 8 ;i++){
-		cout << "Student" << i + 1 << "has the grade of: " << gpas[i] << endl;
+	for (int i = 0; i < size ;i++){
+		cout << "Student " << i + 1 << " has the grade of: " << gpas[i] << endl;
 	}
 	
 	
