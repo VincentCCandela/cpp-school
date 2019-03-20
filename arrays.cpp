16453cc887dd8485825ecddeb3e85c367ade8beb
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-20 13:12:18 -0400

    asdfasdf

--- arrays.cpp
+++ arrays.cpp
@@ -4,6 +4,11 @@ int main(){
 	double gpas[8] = {3.4,34,456,235,4357,1234,3546,4321335};
 	cout << "gpas[1] is: " << gpas[1] << endl;
 	
+	for (int i = 0; i < 8 ;i++){
+		cout << "Student" << i + 1 << "has the grade of: " << gpas[i] << endl;
+	}
+	
+	
 	
 	return 0;
 }
