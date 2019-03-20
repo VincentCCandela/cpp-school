09c7c34b67cbd3ba9bbdb12d501b62670037359f
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-20 13:24:51 -0400

    the arrays program

--- arrays.cpp
+++ arrays.cpp
@@ -14,13 +14,15 @@ int main(){
 		cout << grades[i] << endl;
 	}
 	
-	//double gpas[size] = {3.4,34,456,235,4357,1234,3546,4321335};
-	//cout << "gpas[1] is: " << gpas[1] << endl;
-	//
-	//for (int i = 0; i < size ;i++){
-	//	cout << "Student " << i + 1 << " has the grade of: " << gpas[i] << endl;
-	//}
+	//the old code
 	
+	/*double gpas[size] = {3.4,34,456,235,4357,1234,3546,4321335};
+	cout << "gpas[1] is: " << gpas[1] << endl;
+	
+	for (int i = 0; i < size ;i++){
+		cout << "Student " << i + 1 << " has the grade of: " << gpas[i] << endl;
+	}
+	*/
 	
 	
 	return 0;
