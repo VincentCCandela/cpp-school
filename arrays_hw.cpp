defe78fe29c11be2adc5408ebe46b650cf17718c
Vincent Candela <vcandela21@sl108-21.stjohnsprep.org>
2019-03-22 15:31:49 -0400

    The program for array generation and sign checkage is complete.

--- arrays_hw.cpp
+++ arrays_hw.cpp
@@ -2,53 +2,33 @@
 #include<iomanip>
 using namespace std;
 
-//array of size 50
-//fill array with random numbers from 1-100
-//go through array and count the number of evens and the number of odds
-//output the total number of each
-int evens(a);
-int odds(a);
+//This program:
+//creates an array of size 50,
+//fills the array with random numbers from 1-100.
+//goes through the array and counts the number of evens and the number of odds
+//then outputs the total number of odds and evens.
 
 int main(){
-	srand(time(NULL));
+	srand(time(NULL)); //seed for random numbers is set to random
 
-	int grades[50];
+	int grades[50],evens,odds,i; //array of numbers, and variables for the number of even numbers in the array, the number of odd numbers in the array, and a variable for the iteration in a for loop are declared.
 	
 	
-	for (int i = 0; i < 50; i++){
-		grades[i] = rand()%101;
+	for (i = 0; i < 50; i++){ //a for loop to generate each of the 50 random numbers of the array and check if each of those numbers is odd or even, and lastly to report the sign of each number for a total count of even and odd numbers
 		
-		if((grades[i] % 2) == 0){
-			evens++;
+		grades[i] = rand()%101; //generator for the random numbers
+		cout << grades[i] << endl; // outputs each random number
+		
+		if((grades[i] % 2) == 0){ //checks and reports the signs of each of the numbers
+			evens++; //if the number is even, the the even counter is increased by one
 		}
 		else{
-			odds++
+			odds++; //if the number is odd, the the odd counter is increased by one
 		}
 	}
-	cout << grades[i] << endl;
-	cout << "The number of odds is
-	/*for (int i = 0; i < 50; i++){
-		if((grades[i] % 2) == 0){
-			evens++;
-		}
-		
-		else{
-			odds++;
-		}
-	}*/
-
-
+	
+	cout << "The number of odds is " << odds << endl; //reports the total number of odd numbers
+	cout << "The number of evens is " << evens << endl; // reports the total number of even numberes
+	
 	return 0;
-}
-
-
-int evens(int & evens,int i){
-	if((grades[i] % 2) == 0){
-		evens++;
-	}
-
-}
-
-int odds(int & odds, int i){
-
 }
