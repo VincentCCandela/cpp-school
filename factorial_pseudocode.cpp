dfce56264428ca306ad45a8eee882959173cabfe
Vincent Candela <vcandela21@sl108-11.stjohnsprep.org>
2019-03-28 16:32:42 -0400

    factorial program

---
+++ factorial_pseudocode.cpp
@@ -0,0 +1,8 @@
+integer function taking one unsigned integer parameter {
+	if the inputed parameter is equal to zero or one{
+		return one
+	}
+	else{
+		return multiply the number by a call to the function and input into the the function the number minus one
+	} 
+}
