0ce1e1bc1495ef833a56f668661d863f9ccc8df3
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-03-28 10:18:42 -0400

    an auto upload

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void factorial(int num);
+
+int main(){
+
+cout << "
+
+return 0;
+}
+
+void factorial(int num){
+	if(num == 0){
+		return 1;
+	}
+	else{
+		return
+
+}
