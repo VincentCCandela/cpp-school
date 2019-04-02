cfab518748367e7245d4f1cdcbc8cf22edd6747c
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-04-02 08:42:25 -0400

    bubble sorting program

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 const int size = 20;
 
+void bubbler (int arrary[]);
+
 int main() {
 	srand(time(NULL));
 	
 	int grades[size]
+	
+	cout << "The grades of the function are: ";
 
 	for (int i = 0; i < size ; i++){
 		grades[i] = rand() % 100 + 1
+		cout << grades[i] << endl;
+		
 	}
 
+	cout << endl;
 
 	return 0;
 }
