50bda3a787ca4bd9e5a5d06a4ff9070936015058
Vincent Candela <31104403+VincentCCandela@users.noreply.github.com>
2019-04-02 08:36:21 -0400

    Update sorting_algorithms.cpp

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //sorts an arrays into usable orders
 //three different types
-// 1) Bubble sort
+// 1) Bubble sort (not frequently used because it is the slowest)
 // 2) Selection sort
 // 3) Insertion sort
 // Big-O notation: How efficient and fast an algorithm is. 
