4983bb7e2b48ef094985ab5440a2a33bcd55a18e
Vincent Candela <vcandela21@k215-19.stjohnsprep.org>
2019-04-02 08:39:57 -0400

    bubble sorting program

---
+++ bubble_sort.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+const int size = 20;
+
+int main() {
+	srand(time(NULL));
+	
+	int grades[size]
+
+	for (int i = 0; i < size ; i++){
+		grades[i] = rand() % 100 + 1
+	}
+
+
+	return 0;
+}
